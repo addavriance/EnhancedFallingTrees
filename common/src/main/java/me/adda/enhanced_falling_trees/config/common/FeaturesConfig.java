33e86b56a68bcaee3377e2e58effb349e692bf06
addavriance <61160742+addavriance@users.noreply.github.com>
2024-01-01 11:11:37 +0300

    Disabled chorus by default due to slightly broken behavior

--- common/src/main/java/me/adda/enhanced_falling_trees/config/common/FeaturesConfig.java
+++ common/src/main/java/me/adda/enhanced_falling_trees/config/common/FeaturesConfig.java
@@ -3,5 +3,5 @@ package me.adda.enhanced_falling_trees.config.common;
 public class FeaturesConfig {
 	public boolean disableCactusTrees = false;
 	public boolean disableBambooTrees = false;
-	public boolean disableChorusTrees = false;
+	public boolean disableChorusTrees = true;
 }
