1530f019e77e5dd226b36914476dea14cbedd234
addavriance <netiazhenko@mail.ru>
2024-08-02 04:33:47 +0300

    Fixed bug with "flat" sound of new tree sounds

--- common/src/main/java/me/adda/enhanced_falling_trees/registry/SoundRegistry.java
+++ common/src/main/java/me/adda/enhanced_falling_trees/registry/SoundRegistry.java
@@ -11,15 +11,15 @@ public class SoundRegistry {
 	public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(FallingTrees.MOD_ID, Registries.SOUND_EVENT);
 
 	public static final RegistrySupplier<SoundEvent> TREE_FALL = SOUNDS.register("tree_fall", () ->
-			SoundEvent.createFixedRangeEvent(new ResourceLocation(FallingTrees.MOD_ID, "tree_fall"), 16));
+			SoundEvent.createFixedRangeEvent(new ResourceLocation(FallingTrees.MOD_ID, "tree_fall"), 5));
 	public static final RegistrySupplier<SoundEvent> TREE_IMPACT = SOUNDS.register("tree_impact", () ->
-			SoundEvent.createFixedRangeEvent(new ResourceLocation(FallingTrees.MOD_ID, "tree_impact"), 16));
+			SoundEvent.createFixedRangeEvent(new ResourceLocation(FallingTrees.MOD_ID, "tree_impact"), 5));
 	public static final RegistrySupplier<SoundEvent> CACTUS_FALL = SOUNDS.register("cactus_fall", () ->
-			SoundEvent.createFixedRangeEvent(new ResourceLocation(FallingTrees.MOD_ID, "cactus_fall"), 16));
+			SoundEvent.createFixedRangeEvent(new ResourceLocation(FallingTrees.MOD_ID, "cactus_fall"), 5));
 	public static final RegistrySupplier<SoundEvent> CACTUS_IMPACT = SOUNDS.register("cactus_impact", () ->
-			SoundEvent.createFixedRangeEvent(new ResourceLocation(FallingTrees.MOD_ID, "cactus_impact"), 16));
+			SoundEvent.createFixedRangeEvent(new ResourceLocation(FallingTrees.MOD_ID, "cactus_impact"), 5));
 	public static final RegistrySupplier<SoundEvent> BAMBOO_IMPACT = SOUNDS.register("bamboo_impact", () ->
-			SoundEvent.createFixedRangeEvent(new ResourceLocation(FallingTrees.MOD_ID, "bamboo_impact"), 16));
+			SoundEvent.createFixedRangeEvent(new ResourceLocation(FallingTrees.MOD_ID, "bamboo_impact"), 5));
 	public static final RegistrySupplier<SoundEvent> BAMBOO_FALL = SOUNDS.register("bamboo_fall", () ->
-			SoundEvent.createFixedRangeEvent(new ResourceLocation(FallingTrees.MOD_ID, "bamboo_fall"), 16));
+			SoundEvent.createFixedRangeEvent(new ResourceLocation(FallingTrees.MOD_ID, "bamboo_fall"), 5));
 }
