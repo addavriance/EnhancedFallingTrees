ae9bec1075b0d2afe0326bf93fc28cba993726d5
addavriance <61160742+addavriance@users.noreply.github.com>
2024-11-11 17:27:29 +0300

    fix: Fabric server crash on startup due particles

---
+++ fabric/src/main/java/me/adda/enhanced_falling_trees/fabric/FallingTreesClientFabric.java
@@ -0,0 +1,17 @@
+// FallingTreesFabricClient.java
+package me.adda.enhanced_falling_trees.fabric;
+
+import dev.architectury.registry.client.particle.ParticleProviderRegistry;
+import me.adda.enhanced_falling_trees.particles.LeavesParticles;
+import me.adda.enhanced_falling_trees.registry.ParticleRegistry;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class FallingTreesClientFabric implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        ParticleProviderRegistry.register(ParticleRegistry.LEAVES.get(), LeavesParticles.Factory::new);
+    }
+}
--- fabric/src/main/java/me/adda/enhanced_falling_trees/fabric/FallingTreesFabric.java
+++ fabric/src/main/java/me/adda/enhanced_falling_trees/fabric/FallingTreesFabric.java
@@ -1,8 +1,6 @@
 package me.adda.enhanced_falling_trees.fabric;
 
-import dev.architectury.registry.client.particle.ParticleProviderRegistry;
 import me.adda.enhanced_falling_trees.FallingTrees;
-import me.adda.enhanced_falling_trees.particles.LeavesParticles;
 import me.adda.enhanced_falling_trees.registry.ParticleRegistry;
 import net.fabricmc.api.ModInitializer;
 
@@ -10,10 +8,6 @@ public class FallingTreesFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         ParticleRegistry.bootstrap();
-
-        ParticleProviderRegistry.register(ParticleRegistry.LEAVES.get(), LeavesParticles.Factory::new);
-
         FallingTrees.init();
-
     }
 }
