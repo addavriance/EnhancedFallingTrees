500ec6425e5b11f2e3c3f375d1544e46f686b858
addavriance <61160742+addavriance@users.noreply.github.com>
2026-06-22 00:47:50 +0300

    fix: adapt Jade integration to new IWailaCommonRegistration API

--- common/src/main/java/me/adda/enhanced_falling_trees/compat/Jade.java
+++ common/src/main/java/me/adda/enhanced_falling_trees/compat/Jade.java
@@ -1,15 +1,18 @@
 package me.adda.enhanced_falling_trees.compat;
 
 import me.adda.enhanced_falling_trees.registry.EntityRegistry;
-
-import snownee.jade.api.IWailaClientRegistration;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.EntityType;
+import snownee.jade.api.IWailaCommonRegistration;
 import snownee.jade.api.IWailaPlugin;
 import snownee.jade.api.WailaPlugin;
 
 @WailaPlugin
 public class Jade implements IWailaPlugin {
     @Override
-    public void registerClient(IWailaClientRegistration registration) {
-        registration.hideTarget(EntityRegistry.TREE.get());
+    @SuppressWarnings("unchecked")
+    public void register(IWailaCommonRegistration registration) {
+        ResourceKey<EntityType<?>> key = (ResourceKey<EntityType<?>>) (ResourceKey<?>) EntityRegistry.TREE.getKey();
+        registration.entityTypeOperations().hide(key);
     }
 }
