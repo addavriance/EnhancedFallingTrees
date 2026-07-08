2a0a2ead3e22c13b04ea88a42ebe8fad5be72629
addavriance <61160742+addavriance@users.noreply.github.com>
2026-07-09 02:16:39 +0300

    feat: allow configuring tree-felling tools via item whitelist/blacklist

--- common/src/main/java/me/adda/enhanced_falling_trees/config/common/FilterConfig.java
+++ common/src/main/java/me/adda/enhanced_falling_trees/config/common/FilterConfig.java
@@ -4,12 +4,15 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FilterConfig {
@@ -17,6 +20,8 @@ public class FilterConfig {
 	public FilterBlock log = new FilterBlock(List.of(BlockTags.LOGS), new ArrayList<>(), new ArrayList<>());
 	@ConfigEntry.Gui.CollapsibleObject
 	public FilterBlock leaves = new FilterBlock(List.of(BlockTags.LEAVES), new ArrayList<>(), new ArrayList<>());
+	@ConfigEntry.Gui.CollapsibleObject
+	public FilterItem tool = new FilterItem(List.of(ItemTags.AXES), new ArrayList<>(), new ArrayList<>());
 
 	public static class FilterBlock {
 		public List<String> whitelistedBlockTags;
@@ -56,6 +61,48 @@ public class FilterConfig {
 					.anyMatch(tag -> whitelistedBlockTags.contains(tag.location().toString()));
 
 			boolean inWhitelist = whitelistedBlocks.contains(blockName);
+
+			return matchesTag || inWhitelist;
+		}
+	}
+
+	public static class FilterItem {
+		public List<String> whitelistedItemTags;
+		public List<String> whitelistedItems;
+		public List<String> blacklistedItems;
+
+		public FilterItem(List<TagKey<Item>> whitelistedItemTags, List<Item> whitelistedItems, List<Item> blacklistedItems) {
+			this.whitelistedItemTags = whitelistedItemTags.stream()
+					.map(itemTagKey -> itemTagKey.location().toString())
+					.collect(Collectors.toCollection(ArrayList::new));
+
+			this.whitelistedItems = whitelistedItems.stream()
+					.map(item -> BuiltInRegistries.ITEM.getKey(item).toString())
+					.collect(Collectors.toCollection(ArrayList::new));
+
+			this.blacklistedItems = blacklistedItems.stream()
+					.map(item -> BuiltInRegistries.ITEM.getKey(item).toString())
+					.collect(Collectors.toCollection(ArrayList::new));
+		}
+
+		public FilterItem() {
+			// Пустой конструктор для autoconfig
+			this.whitelistedItemTags = new ArrayList<>();
+			this.whitelistedItems = new ArrayList<>();
+			this.blacklistedItems = new ArrayList<>();
+		}
+
+		public boolean isValid(Item item) {
+			Identifier itemId = BuiltInRegistries.ITEM.getKey(item);
+			String itemName = itemId.toString();
+
+			if (blacklistedItems.contains(itemName))
+				return false;
+
+			boolean matchesTag = BuiltInRegistries.ITEM.wrapAsHolder(item).tags()
+					.anyMatch(tag -> whitelistedItemTags.contains(tag.location().toString()));
+
+			boolean inWhitelist = whitelistedItems.contains(itemName);
 
 			return matchesTag || inWhitelist;
 		}
