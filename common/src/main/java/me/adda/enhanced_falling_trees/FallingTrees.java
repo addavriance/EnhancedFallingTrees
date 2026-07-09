f10298714efdc44d4664f9819f127c29245982e9
addavriance <61160742+addavriance@users.noreply.github.com>
2026-07-09 04:58:01 +0300

    fix: replace Player synced-data mixin with a plain UUID map for client config

--- common/src/main/java/me/adda/enhanced_falling_trees/FallingTrees.java
+++ common/src/main/java/me/adda/enhanced_falling_trees/FallingTrees.java
@@ -9,12 +9,7 @@ import me.adda.enhanced_falling_trees.registry.ParticleRegistry;
 import me.adda.enhanced_falling_trees.registry.SoundRegistry;
 import me.adda.enhanced_falling_trees.registry.TreeTypeRegistry;
 import me.adda.enhanced_falling_trees.utils.BlockMapEntityData;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.player.Player;
 
 public class FallingTrees {
 	public static final String MOD_ID = "efallingtrees";
@@ -32,7 +27,4 @@ public class FallingTrees {
 		PlatformServices.REGISTRATION.registerEntityDataSerializer(
 				ResourceLocation.fromNamespaceAndPath(MOD_ID, "block_map"), BlockMapEntityData.BLOCK_MAP);
 	}
-
-	public static final EntityDataAccessor<CompoundTag> PLAYER_CLIENT_CONFIG =
-			SynchedEntityData.defineId(Player.class, EntityDataSerializers.COMPOUND_TAG);
 }
